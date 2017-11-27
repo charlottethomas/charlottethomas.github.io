c6ede017d71e8f1016e041d1b5074530e29b362c
Clement Lepissier <clem.lepissier@gmail.com>
2017-11-27 14:34:20 +0100

    v1.1.0

--- js/app.js
+++ js/app.js
@@ -3,34 +3,18 @@ window.onload=function(){
   var elements = document.getElementsByClassName("is-zoomable")
   var nZoomables = elements.length
   var zoomables = []
-
-  function toggleZoomable(ev) {
-    var toggeled = zoomables.find(function (zoomable) {
-      return zoomable.id === ev.target.id
-    })
-    toggeled.active ? toggeled.imgZoom.disable() : toggeled.imgZoom.enable()
-    toggeled.active = !toggeled.active
-  }
-
+  console.log('elements :', elements)
   for (var i = 0; i < nZoomables; i++) {
-    elements[i].onclick = toggleZoomable
     zoomables.push({
       id: elements[i].id,
-      // element: elements[i],
-      // imgZoom: new ImageZoom('#'+elements[i].id),
       active: false
     })
   }
 
   zoomables.forEach(function(zoomable) {
-    zoomable.imgZoom = new ImageZoom('#'+zoomable.id)
-    setTimeout(function () {zoomable.imgZoom.disable()}, 500);
-    // zoomable.imgZoom.disable()
+    zoomable.imgZoom = new ImageZoom('#'+zoomable.id, {
+      defaultDisabled: true,
+      toggleOnClick: true
+    });
   })
-
-  // setTimeout(function() {
-  //   zoomables.forEach(function(zoomable) {
-  //     zoomable.imgZoom.disable()
-  //   })
-  // }, 500)
 }
